a6774df2d62d6a59121d6fbb8555ed5d031c1fab
Matt Teichman <teichman@uchicago.edu>
2021-11-23 11:59:30 -0600

    still tinkering...

--- lib/lib.ml
+++ lib/lib.ml
@@ -105,24 +105,5 @@ module Commands = struct
 
   let create_dune_project name =
     echo (dune_project name) > "dune-project"
-
-  module IO = struct
-    
-    let mk_project_root name =
-      mk_project name |> Feather.run
-
-    let inside_the_dir name =
-      foldl1 and_ [
-          delete_bin
-        ; init_executable name
-        ; create_lib
-        ; create_lib_dune
-        ; create_exe name
-        ; create_exe_dune name
-        ; create_use_output
-        ; create_ocamlinit
-        ; create_dune_project name
-        ] |> Feather.run ~cwd:name
-  end
     
 end
--- spinup.ml
+++ spinup.ml
@@ -1,10 +1,33 @@
-module Main = struct
-  open Lib.Commands.IO
+open Prelude
 
-  let prog name =
+module Main = struct
+
+  module Feather2IO = struct
+    open Feather
+    open Lib.Commands
+
+    let mk_project_root name =
+      mk_project name |> Feather.run
+      
+    let inside_the_dir name =
+      foldl1 and_ [
+          delete_bin
+        ; init_executable name
+        ; create_lib
+        ; create_lib_dune
+        ; create_exe name
+        ; create_exe_dune name
+        ; create_use_output
+        ; create_ocamlinit
+        ; create_dune_project name
+        ] |> Feather.run ~cwd:name
+  end
+  open Feather2IO
+            
+  let main name =
     mk_project_root name
     ; inside_the_dir name
 
 end
               
-let () = Main.prog "matt"
+let () = Main.main "matt"
