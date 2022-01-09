9547c0e4c2951c1ab66c1a5bf17287bcf67aab0f
Keith Waclena <keith@lib.uchicago.edu>
2022-01-09 13:32:07 -0600

    added new GNUmakefile
    also tweaked away some unnecessary ignores and @@'s, sorry!

--- io.ml
+++ io.ml
@@ -6,63 +6,60 @@ module SmallCommands = struct
 
   module Verbosity = struct
     let verbose = true
-                
+
     let verbose_print msg =
       if verbose
       then print msg
       else ()
   end
   open Verbosity
-     
+
   let mk_project name =
     runfull [ "dune" ; "init" ; "project" ; name ]
     |> ignore ;
     verbose_print @@ Messages.mk_project name
-    
+
   let delete_bin () =
     runfull [ "rm" ; "-R" ; "bin" ]
     |> ignore ;
-    verbose_print @@ Messages.delete_bin
+    verbose_print Messages.delete_bin
 
   let init_executable name =
     runfull [ "dune" ; "init" ; "exe" ; name ]
     |> ignore ;
-    verbose_print @@ Messages.init_executable
+    verbose_print Messages.init_executable
 
   let create_lib () =
-    writefile ~fn:lib_path lib
-    |> ignore ;
-    verbose_print @@ Messages.create_lib
+    writefile ~fn:lib_path lib;
+    verbose_print Messages.create_lib
 
   let create_lib_dune () =
-    writefile ~fn:lib_dune_path lib_dune
-    |> ignore ;
-    verbose_print @@ Messages.create_lib_dune
+    writefile ~fn:lib_dune_path lib_dune;
+    verbose_print Messages.create_lib_dune
 
   let create_exe name =
-    writefile ~fn:(name ^ ".ml") exe
-    |> ignore ;
+    writefile ~fn:(name ^ ".ml") exe;
     verbose_print @@ Messages.create_exe name
 
   let create_exe_dune name =
-    writefile ~fn:dune_path (exe_dune name)
-    |> ignore ;
-    verbose_print @@ Messages.create_exe_dune
+    writefile ~fn:dune_path (exe_dune name);
+    verbose_print Messages.create_exe_dune
 
   let create_use_output () =
-    writefile ~fn:use_output_path use_output
-    |> ignore ;
-    verbose_print @@ Messages.create_use_output
+    writefile ~fn:use_output_path use_output;
+    verbose_print Messages.create_use_output
 
   let create_ocamlinit () =
-    writefile ~fn:ocamlinit_path ocamlinit
-    |> ignore ;
-    verbose_print @@ Messages.create_ocamlinit
+    writefile ~fn:ocamlinit_path ocamlinit;
+    verbose_print Messages.create_ocamlinit
 
   let create_dune_project name =
-    writefile ~fn:dune_project_path (dune_project name)
-    |> ignore ;
-    verbose_print @@ Messages.create_dune_project
+    writefile ~fn:dune_project_path (dune_project name);
+    verbose_print Messages.create_dune_project
+
+  let create_gnumakefile () =
+    writefile ~fn:gnumakefile_path gnumakefile;
+    verbose_print Messages.create_gnumakefile
 
   let done_msg () = print Messages.done_msg
 end
@@ -71,7 +68,7 @@ open SmallCommands
 module BigPicture = struct
   let mk_project_root name =
     mk_project name
-    
+
   let inside_the_dir name =
     delete_bin () ;
     init_executable name ;
@@ -82,6 +79,7 @@ module BigPicture = struct
     create_use_output () ;
     create_ocamlinit () ;
     create_dune_project name ;
+    create_gnumakefile ();
     done_msg ()
 
   let the_whole_thing name =
