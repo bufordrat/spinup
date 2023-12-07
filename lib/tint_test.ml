16e9882fd3d557d8434b34c15ab409f360be02e3
Matt Teichman <teichman@uchicago.edu>
2023-12-07 16:32:45 -0600

    experiments coming along

--- lib/tint_test.ml
+++ lib/tint_test.ml
@@ -1,42 +1,40 @@
-module Original = struct
-  let eval state line =
-    match Tint.Eval.(eval (reset state) line) with
-    | Error err          -> Tint.Types.string_of_error err |> prerr_endline; state
-    | Ok (state, result) -> print_endline result; state
+module Setup = struct
+  let initialize context =
+    let open Tint.Eval in
+    let open Etude.Result.Make (String) in
+    let* syntax = Tint.Types.Syntax.of_string "#[,]" in
+    init ~syntax:syntax prims (Forms.forms context)
 
-  let each_inline () =
-    match In_channel.input_line stdin with
+  let each_inline channel =
+    match In_channel.input_line channel with
     | None      -> None
-    | Some line -> Some (line, ())
-
-  let main () =
-    match Tint.Eval.(init prims (Forms.forms ["os",Sys.os_type])) with
-    | Error err -> prerr_endline err; exit 1
-    | Ok state  -> Seq.unfold each_inline () |> Seq.fold_left eval state |> ignore
+    | Some line -> Some (line, channel)
 end
 
-let syntax = Tint.Types.Syntax.of_string "#[,]"
+module Context = struct
+  let context = [ "pname", "example" ; ]
+end
 
-let context = [ "pname", "example" ; ]
+module Channel = struct
+  let eval oc state line =
+    match Tint.Eval.(eval (reset state) line) with
+    | Error err          -> Tint.Types.string_of_error err |> prerr_endline; state
+    | Ok (state, result) -> Prelude.(without (flip writeline oc) result) ; state
 
-let initialize context =
-  let open Tint.Eval in
-  init ~syntax:syntax prims (Forms.forms context)
+  let write_channels context ic oc =
+    let open Setup in
+    match initialize context with
+    | Error err -> prerr_endline err; exit 1
+    | Ok state  -> Seq.unfold each_inline ic
+                   |> Seq.fold_left (eval oc) state
+                   |> ignore
 
-let eval state line =
-  match Tint.Eval.(eval (reset state) line) with
-  | Error err          -> Tint.Types.string_of_error err |> prerr_endline; state
-  | Ok (state, result) -> print_endline result; state
+  let write_p context filepath oc =
+    let open_template c =
+      write_channels context c oc
+    in Prelude.within open_template filepath
+end
 
-let each_inline channel =
-  match In_channel.input_line channel with
-  | None      -> None
-  | Some line -> Some (line, channel)
-
-let main channel =
-  match initialize context with
-  | Error err -> prerr_endline err; exit 1
-  | Ok state  -> Seq.unfold each_inline channel |> Seq.fold_left eval state |> ignore
 
 (* let reducer state line =
  *   match Tint.Eval.(eval (reset state) line) with
