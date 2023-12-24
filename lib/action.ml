b4ae6b9298c5428186c3a171b0d7041882a73efe
Matt Teichman <teichman@uchicago.edu>
2023-12-23 21:21:34 -0800

    rearrange the project a little

--- lib/action.ml
+++ lib/action.ml
@@ -31,93 +31,62 @@ module Dirs = struct
       cmessage = "making test/ directory..." ; }
 end
 
-let mk_project name =
-  Command.
-  { args = [ "dune" ; "init" ; "project" ; name ] ;
-    cmessage = "creating " ^ name ^ " project..." ; }
+module Conclude = struct
+  let do_a_build =
+    Command.
+    { args = [ "dune" ; "build" ] ;
+      cmessage =
+        "doing initial `dune build` to generate .opam file..." ; }
 
-let delete_bin =
-  Command.
-  { args = [ "rm" ; "-r" ; "bin" ] ;
-    cmessage = "creating library dune config..." ; }
+  let do_a_clean =
+    Command.
+    { args = [ "dune" ; "clean" ] ;
+      cmessage =
+        "doing a `dune clean` to remove compiler detritus..." ; }
 
-let init_executable name =
-  Command.
-  { args = [ "dune" ; "init" ; "exe" ; name ] ;
-    cmessage =
-      "creating executable module " ^ name ^ ".ml..." ; }
+  let done_msg =
+    Command.
+    { args = [] ;
+      cmessage = "DONE!" ; }
 
-let do_a_build =
-  Command.
-  { args = [ "dune" ; "build" ] ;
-    cmessage =
-      "doing initial `dune build` to generate .opam file..." ; }
-
-let do_a_clean =
-  Command.
-  { args = [ "dune" ; "clean" ] ;
-    cmessage =
-      "doing a `dune clean` to remove compiler detritus..." ; }
-
-let done_msg =
-  Command.
-  { args = [] ;
-    cmessage = "DONE!" ; }
-
-let sandbox_msg name =
-  let msg =
-    Prelude.sprintf "\nto install project dependencies into the current opam \
-                     switch, run this command inside the %s/ directory:\n\
-                     \n\
-                     \  $ make deps\n\
-                     \n\
-                     to create a sandboxed opam switch, run this command \
-                     inside the %s/ directory:\n\
-                     \n\
-                     \  $ make sandbox\n" name name
-  in
-  Command.
-  { args = [] ;
-    cmessage = msg ; }
-
-(* TODO: fix this so that the file writes are processed *)
+  let sandbox_msg name =
+    let msg =
+      Prelude.sprintf "\nto install project dependencies into the current opam \
+                       switch, run this command inside the %s/ directory:\n\
+                       \n\
+                       \  $ make deps\n\
+                       \n\
+                       to create a sandboxed opam switch, run this command \
+                       inside the %s/ directory:\n\
+                       \n\
+                       \  $ make sandbox\n" name name
+    in
+    Command.
+    { args = [] ;
+      cmessage = msg ; }
+end
 
 let main_actions name =
   let open Template in
   let open Etude.Result.Make (String) in
-  let dune_commands1 = [
-      Run delete_bin ;
-      Run (init_executable name) ;
-    ]
-  in
-  let dune_commands2 = [
-      Run do_a_build ;
-      Run do_a_clean ;
-      Run done_msg ;
-      Run (sandbox_msg name) ; ]
+  let make_dirs = Dirs.[
+        Run mk_appdir ;
+        Run mk_libdir ;
+        Run mk_testdir ; ]
   in
   let+ processed =
-    traverse Processed.process (FromFiles.files name)
+    traverse
+      Processed.process
+      (FromFiles.files name)
   in
   let writes =
     List.map write processed
+  in  
+  let finish_up = Conclude.[
+        Run do_a_build ;
+        Run do_a_clean ;
+        Run done_msg ;
+        Run (sandbox_msg name) ; ]
   in
-  dune_commands1 @ writes @ dune_commands2
-
-(* let inside_the_dir name =
- *   let open Template.FromFiles in
- *   let dune_commands1 = [
- *       Run delete_bin ;
- *       Run (init_executable name) ;
- *     ] inp
- *   let file_writes =
- *     List.map write (files name)
- *   in
- *   let dune_commands2 = [
- *       Run do_a_build ;
- *       Run do_a_clean ;
- *       Run done_msg ;
- *       Run (sandbox_msg name) ; ]
- *   in
- *   dune_commands1 @ file_writes @ dune_commands2 *)
+  make_dirs @ writes @ finish_up
       
