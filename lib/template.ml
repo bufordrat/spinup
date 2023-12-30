64e1fc6c00c8c93a85fcd947708c6c7f54f21454
Matt Teichman <teichman@uchicago.edu>
2023-12-29 20:34:12 -0800

    module R in template.ml

--- lib/template.ml
+++ lib/template.ml
@@ -1,3 +1,5 @@
+module R = Etude.Result.Make (String)
+
 module Path = struct
   let path = "/home/teichman/.config/spinup/templates"
 end
@@ -6,12 +8,12 @@ module Engine = struct
   let default_syntax = Tint.Types.Syntax.tracstring
 
   let context_to_state ?(syntax=default_syntax) context =
-    let open Etude.Result.Make (String) in
+    let open R in
     let* syntax = Tint.Types.Syntax.of_string syntax in
     Tint.Eval.(init ~syntax prims (Forms.forms context))
 
   let macro_expand ?syntax ~context tint =
-    let open Etude.Result.Make (String) in
+    let open R in
     let* state = context_to_state ?syntax context in
     let+ (_, processed_string) =
       map_error
@@ -20,7 +22,7 @@ module Engine = struct
     in processed_string
 
   let process_template_path path template context =
-    let open Etude.Result.Make (String) in
+    let open R in
     let fullpath = path ^ "/" ^ template in
     let* raw_contents =
       let open Prelude in
