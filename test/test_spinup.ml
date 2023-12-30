5a8bb2f4b349ab42d44b8292de644de49bf6e829
Matt Teichman <teichman@uchicago.edu>
2023-12-30 15:04:46 -0800

    ok, sweet, unit tests are finally working

--- test/test_spinup.ml
+++ test/test_spinup.ml
@@ -1,19 +1,22 @@
 let dune_project_is_there () =
   let open Alcotest in
+  let dp = Lib.Crunched.read "dune-project" in
   (check (neg (option string)))
-    (Some "hello")
+    "crunched file is present"
+    dp
     None
 
-let dude () =
+let gnumakefile_is_there () =
   let open Alcotest in
-  (check string) "same string" "dude" "dude"
-
-let man () =
-  let open Alcotest in
-  (check string) "same string" "man" "man"
+  let dp = Lib.Crunched.read "GNUmakefile" in
+  (check (neg (option string)))
+    "crunched file is present"
+    dp
+    None
 
 let () =
   let open Alcotest in 
   run "Spinup Tests" [
-      "check crunched dir", [ test_case "dune-project present" `Quick dune_project_is_there ; ] ;
+      "check crunched dir", [ test_case "dune-project is present" `Quick dune_project_is_there ;
+                              test_case "GNUmakefile is present" `Quick gnumakefile_is_there ; ] ;
     ]
