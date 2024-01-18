15af30111cedb8f7b7f69912928bc2123de0dae1
Matt Teichman <teichman@uchicago.edu>
2024-01-17 21:24:26 -0600

    flesh out config a tiny bit

--- lib/config.ml
+++ lib/config.ml
@@ -1,17 +1,34 @@
 type t = { description : string ;
            synopsis : string ;
+           maintainers : string list ;
+           authors : string list ;
            homepage : string ;
+           bug_reports : string ;
          }
 
-let default_config =
+let default =
   let description = "Insert project description here." in
   let synopsis = "Insert project synopsis, which is supposedly \
                   different, here" in
+  let maintainers = [ "Your Name <youremail@gmail.com>" ] in
+  let authors = [ "Your Name <youremail@gmail.com>" ] in
   let homepage = "https://your.website.here" in
-  { description ; synopsis ; homepage }
+  let bug_reports = "https://your.website.here" in
+  { description ;
+    synopsis ;
+    maintainers ;
+    authors ;
+    homepage ;
+    bug_reports }
 
-let default_context ?(config=default_config) pname =
+let default_context ?(config=default) pname =
+  let open Prelude.String in
   [ "pname", pname ;
     "description", config.description ;
     "synopsis", config.synopsis ;
-    "homepage", config.homepage ; ]
+    "maintainers", join ~sep:" " config.maintainers ;
+    "authors", join ~sep:" " config.authors ;
+    "homepage", config.homepage ;
+    "bug_reports", config.bug_reports ; ]
+
+
