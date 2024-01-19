93e460d45c2863a8ab9f66b12814d528282503b8
Matt Teichman <teichman@uchicago.edu>
2024-01-18 20:26:32 -0600

    some style tweaks

--- lib/config.ml
+++ lib/config.ml
@@ -7,14 +7,18 @@ let default pname =
   let context = default_context pname
   in { context }
 
-let moar_parsing str =
-  let module E = struct type t = int * string end in
+let refer_parse str =
+  let module E =
+    struct type t = int * string end
+  in
   let open Etude.Result.Make (E) in
-  let open Prelude.Refer.Seq in
   let collapse db =
     let open Etude.List in
     let each_pair (_, assocs) = assocs in
     db >>= each_pair
   in
-  let lst = Prelude.Seq.to_list (of_string str) in
+  let lst =
+    let open Prelude in
+    Seq.to_list (Refer.Seq.of_string str)
+  in
   sequence lst >>| collapse
