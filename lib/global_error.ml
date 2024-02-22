d8d23814f796f8280fed63227923a101ba88ae9b
Matt Teichman <teichman@uchicago.edu>
2024-02-22 13:21:05 -0600

    aha, there was a bug in .ocamlformat

--- lib/global_error.ml
+++ lib/global_error.ml
@@ -2,24 +2,31 @@ module Errlist = Global_error_intf.Errlist
 
 module type TRACE = Global_error_intf.TRACE
 
-module T =
-  struct
-    type 'a trace = ('a, Global_error_intf.Errlist.t) result
-    let coerce e = (e : [< Global_error_intf.global_error ]
-                        :> Global_error_intf.global_error)
-    let new_error e =
-      Error [coerce e]
-    let with_error e = function
-      | Ok _ as o -> o
-      | Error lst -> Error (coerce e :: lst)
-  end
+module T = struct
+  type 'a trace = ('a, Global_error_intf.Errlist.t) result
 
-module Specialize (E : sig type error end) = struct
-  type 'a new_error = E.error ->
-                      ('a, Errlist.t) result
-  type 'a with_error = E.error ->
-                     ('a, Errlist.t) result ->
-                     ('a, Errlist.t) result
-  end
+  let coerce e =
+    ( e
+      : [< Global_error_intf.global_error]
+      :> Global_error_intf.global_error )
+
+  let new_error e = Error [ coerce e ]
+
+  let with_error e = function
+    | Ok _ as o -> o
+    | Error lst -> Error (coerce e :: lst)
+end
+
+module Specialize (E : sig
+  type error
+end) =
+struct
+  type 'a new_error = E.error -> ('a, Errlist.t) result
+
+  type 'a with_error =
+    E.error ->
+    ('a, Errlist.t) result ->
+    ('a, Errlist.t) result
+end
 
 let export = Fun.id
