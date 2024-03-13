127d49ba2c39876cb7042e7faf628bdef6a4287b
Matt Teichman <teichman@uchicago.edu>
2024-03-13 14:36:33 -0500

    template.ml is looking double-prime-ified

--- lib/template.ml
+++ lib/template.ml
@@ -2,6 +2,7 @@ module E = Template_error
 module Ty = Tint.Types
 module R = Etude.Result.Make (String)
 module R' = Etude.Result.Make (E)
+module R'' = Etude.Result.Make (Global_error)
 
 module Engine = struct
   let spinup_syntax = "#[,]"
@@ -17,6 +18,12 @@ module Engine = struct
     let open Ty.Syntax in
     map_error syntax_string (of_string s)
 
+  let string_to_syntax'' s =
+    let open E.Smart in
+    let open Ty.Syntax in
+    let new_error str = [ syntax_string str ] in
+    map_error new_error (of_string s)
+
   let context_to_state' ?(syntax = spinup_syntax) context =
     (* note: when TINT updates so that init doesn't return a
        result, this code will need to be modified *)
@@ -27,6 +34,18 @@ module Engine = struct
       Tint.Eval.(init ~syntax prims (Forms.forms context))
     in
     map_error syntax_string string_version
+
+  let context_to_state'' ?(syntax = spinup_syntax) context =
+    (* note: when TINT updates so that init doesn't return a
+       result, this code will need to be modified *)
+    let open R'' in
+    let open E.Smart in
+    let* syntax = string_to_syntax'' syntax in
+    let string_version =
+      Tint.Eval.(init ~syntax prims (Forms.forms context))
+    in
+    let new_error str = [ syntax_string str ] in
+    map_error new_error string_version
 
   let macro_expand ?(syntax = spinup_syntax) ~context tint =
     let open R in
@@ -52,12 +71,30 @@ module Engine = struct
     in
     processed_string
 
+  let macro_expand'' ?(syntax = spinup_syntax) ~context tint
+      =
+    let open R'' in
+    let open E.Smart in
+    let* state = context_to_state'' ~syntax context in
+    let new_error msg = [ tint_syntax msg ] in
+    let prep_error tint_error =
+      Ty.error_message state.syntax tint_error |> new_error
+    in
+    let+ _, processed_string =
+      map_error prep_error (Tint.Eval.eval state tint)
+    in
+    processed_string
+
   let expand_string ?(syntax = spinup_syntax) ~context str =
     macro_expand ~syntax ~context str
 
   let expand_string' ?(syntax = spinup_syntax) ~context str
       =
     macro_expand' ~syntax ~context str
+
+  let expand_string'' ?(syntax = spinup_syntax) ~context str
+      =
+    macro_expand'' ~syntax ~context str
 
   let expand_crunched ~template ~context =
     let open R in
@@ -73,6 +110,12 @@ module Engine = struct
     | Some contents -> Ok contents
     | None -> Error (template_crunch path)
 
+  let option_to_result'' path =
+    let open E.Smart in
+    function
+    | Some contents -> Ok contents
+    | None -> Error [ template_crunch path ]
+
   let expand_crunched' ~template ~context =
     let open R' in
     let* raw_contents =
@@ -80,6 +123,14 @@ module Engine = struct
       |> option_to_result' template
     in
     expand_string' ~context raw_contents
+
+  let expand_crunched'' ~template ~context =
+    let open R'' in
+    let* raw_contents =
+      Crunched_templates.read template
+      |> option_to_result'' template
+    in
+    expand_string'' ~context raw_contents
 end
 
 module Processed = struct
@@ -143,6 +194,25 @@ module Unprocessed = struct
       umessage
     }
 
+  let expand_filenames'' unp =
+    let open R'' in
+    let open Engine in
+    let context = unp.context in
+    let template_filename = unp.template_filename in
+    let template_path = unp.template_path in
+    let+ output_filename =
+      expand_string'' ~context unp.output_filename
+    and+ output_path =
+      expand_string'' ~context unp.output_path
+    and+ umessage = expand_string'' ~context unp.umessage in
+    { template_filename;
+      output_filename;
+      template_path;
+      output_path;
+      context;
+      umessage
+    }
+
   let process unp =
     let open R in
     let context = unp.context in
@@ -187,6 +257,30 @@ module Unprocessed = struct
     in
     let+ data =
       Engine.expand_crunched' ~template:template_path
+        ~context
+    in
+    Processed.{ write_path; data; vmessage }
+
+  let process'' unp =
+    let open R'' in
+    let context = unp.context in
+    let* partial = expand_filenames'' unp in
+    let write_path =
+      let open Prelude in
+      match partial.output_path with
+      | "" -> Prelude.File.join "." partial.output_filename
+      | other ->
+        String.join ~sep:"/"
+          [ "."; other; partial.output_filename ]
+    in
+    let vmessage = partial.umessage in
+    let template_path =
+      let open Prelude in
+      String.join ~sep:"/"
+        [ partial.output_path; partial.template_filename ]
+    in
+    let+ data =
+      Engine.expand_crunched'' ~template:template_path
         ~context
     in
     Processed.{ write_path; data; vmessage }
