03aff7f60611e34817364534f1d246b32a4a06a2
Matt Teichman <teichman@uchicago.edu>
2024-03-21 18:01:19 -0500

    notes for putting in grep format

--- lib/global_error.ml
+++ lib/global_error.ml
@@ -8,6 +8,7 @@ let t_to_string =
   let open Printf in
   function
   | `ReferParsing (i, s) ->
+    (* this needs the grep format *)
     sprintf "Refer parsing error, line %i:\n%s" i s
   | `ConfigCrunch s ->
     sprintf "Config crunch filepath not found:\n%s" s
@@ -16,7 +17,10 @@ let t_to_string =
     sprintf "Directory already exists:\n%s" s
   | `SyntaxString s ->
     sprintf "Bad TINT syntax string:\n%s" s
-  | `TintSyntax s -> sprintf "TINT error:\n%s" s
+  | `TintSyntax s -> 
+     (* once I fix the error in Lib.Template.macro_expand'' so that it
+        isn't a string, give this branch the grep format as well *)
+     sprintf "TINT error:\n%s" s
   | `TemplateCrunch s ->
     sprintf "Template crunch filepath not found:\n%s" s
   | `TemplateErr -> "Template error"
