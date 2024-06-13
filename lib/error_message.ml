5110700f6bb9b3bc6c079eb9eaccb226bf70e0ef
Matt Teichman <teichman@uchicago.edu>
2024-06-13 14:48:32 -0500

    parse errors are a kind of global error; hopefully that doesn't end up biting me

--- lib/error_message.ml
+++ lib/error_message.ml
@@ -3,14 +3,15 @@ module R = Etude.Result.Make (String)
 module Parser = struct
   module ParserMonad = struct
     type 'a t =
-      Global_error.t -> ('a * Global_error.t, string) result
+      Global_error.t ->
+      ('a * Global_error.t, Global_error.t) result
 
     let pure a input = Ok (a, input)
 
     let bind mx k input =
       match mx input with
       | Ok (result, remainder) -> (k result) remainder
-      | Error _ -> Error "error"
+      | Error e -> Error e
   end
 
   include Etude.Endofunctors.Monad.Make (ParserMonad)
