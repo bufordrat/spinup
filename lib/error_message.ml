d2a2dd184d5a180a5686a6cfeb08d0b961bcb16c
Matt Teichman <teichman@uchicago.edu>
2024-06-27 11:43:43 -0500

    still in the middle of getting Error_message to compile again

--- lib/error_message.ml
+++ lib/error_message.ml
@@ -35,7 +35,7 @@ module MessageError = struct
   type t = Message.error
 end
 
-module R = Etude.Result.Make (Message)
+module R = Etude.Result.Make (MessageError)
 
 module Parser = struct
   module P = struct
@@ -43,7 +43,7 @@ module Parser = struct
 
     type 'a t =
       Global_error.t ->
-      ('a * Global_error.t, message) result
+      ('a * Global_error.t, Message.error) result
   end
 
   module ParserMonad = struct
@@ -72,24 +72,25 @@ module Parser = struct
       | Error _ -> prsr2 input
   end
 
-  include Etude.Monoid.Make (ParserAlternative)
+  (* include Etude.Monoid.Make (ParserAlternative) *)
 
-  let run prsr input = prsr input
-  let eval prsr input = R.map fst (prsr input)
-  let exec prsr input = R.map snd (prsr input)
+  (* let run prsr input = prsr input *)
+  (* let eval prsr input = R.map fst (prsr input) *)
+  (* let exec prsr input = R.map snd (prsr input) *)
 
-  let satisfy pred =
-    let open Message in
-    function
-    | tok :: toks ->
-      if pred tok
-      then Ok (tok, toks)
-      else
-        Error (ErrorMessageParse ("satisfy", Some tok, toks))
-    | [] ->
-      Error (ErrorMessageParse ("end of input", None, []))
+  (* let satisfy pred = *)
+  (*   let open Message in *)
+  (*   function *)
+  (*   | tok :: toks -> *)
+  (*     if pred tok *)
+  (*     then Ok (tok, toks) *)
+  (*     else *)
+  (* Error (ErrorMessageParse ("satisfy", Some tok,
+     toks)) *)
+  (* | [] -> *)
+  (* Error (ErrorMessageParse ("end of input", None, [])) *)
 
-  let optional prsr = prsr <|> pure ()
+  (* let optional prsr = prsr <|> pure () *)
 end
 
 let example1 =
@@ -111,69 +112,70 @@ let example2 =
         "keith" )
   ]
 
-module Parsers = struct
-  [@@@warning "-8"]
+(* module Parsers = struct *)
+(*   [@@@warning "-8"] *)
 
-  let config_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ _ = satisfy is_config_err in
-    Config
+(*   let config_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ _ = satisfy is_config_err in *)
+(*     Config *)
 
-  let template_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ _ = satisfy is_template_err in
-    Template
+(*   let template_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ _ = satisfy is_template_err in *)
+(*     Template *)
 
-  let filesystem_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ _ = satisfy is_filesystem_err in
-    Filesystem
+(*   let filesystem_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ _ = satisfy is_filesystem_err in *)
+(*     Filesystem *)
 
-  let application_layer_parser =
-    let open Parser in
-    config_parser <|> template_parser <|> filesystem_parser
+(*   let application_layer_parser = *)
+(*     let open Parser in *)
+(* config_parser <|> template_parser <|>
+   filesystem_parser *)
 
-  let refer_error_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ layer = application_layer_parser
-    and+ (`ReferError (datasource, line, msg)) =
-      satisfy is_refer_error
-    in
-    ReferError (layer, datasource, line, msg)
+(*   let refer_error_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ layer = application_layer_parser *)
+(*     and+ (`ReferError (datasource, line, msg)) = *)
+(*       satisfy is_refer_error *)
+(*     in *)
+(*     ReferError (layer, datasource, line, msg) *)
 
-  let tint_syntax_error_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ layer = application_layer_parser
-    and+ (`TintSyntax ({ path; _ } as ts)) =
-      satisfy is_tint_syntax
-    in
-    TintSyntaxError (FromCrunch path, layer, ts)
+(*   let tint_syntax_error_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ layer = application_layer_parser *)
+(*     and+ (`TintSyntax ({ path; _ } as ts)) = *)
+(*       satisfy is_tint_syntax *)
+(*     in *)
+(*     TintSyntaxError (FromCrunch path, layer, ts) *)
 
-  let already_exists_parser =
-    let open Parser in
-    let open Message in
-    let open Global_error.Smart in
-    let+ layer = application_layer_parser
-    and+ (`AlreadyExists (path, dof, pname)) =
-      satisfy is_already_exists
-    in
-    AlreadyExists (layer, path, dof, pname)
+(*   let already_exists_parser = *)
+(*     let open Parser in *)
+(*     let open Message in *)
+(*     let open Global_error.Smart in *)
+(*     let+ layer = application_layer_parser *)
+(*     and+ (`AlreadyExists (path, dof, pname)) = *)
+(*       satisfy is_already_exists *)
+(*     in *)
+(*     AlreadyExists (layer, path, dof, pname) *)
 
-  let parse =
-    let open Parser in
-    refer_error_parser
-    <|> tint_syntax_error_parser
-    <|> already_exists_parser
-end
+(*   let parse = *)
+(*     let open Parser in *)
+(*     refer_error_parser *)
+(*     <|> tint_syntax_error_parser *)
+(*     <|> already_exists_parser *)
+(* end *)
 
-let parse = Parsers.parse
+(* let parse = Parsers.parse *)
