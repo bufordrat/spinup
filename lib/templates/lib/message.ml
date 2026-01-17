89632eeb5da24d0a55befeb88000cef0ff73e0a6
Matt Teichman <teichman@uchicago.edu>
2026-01-16 22:20:03 -0600

    don't mention the brackets

--- lib/templates/lib/message.ml
+++ lib/templates/lib/message.ml
@@ -1,14 +1,17 @@
-let response = 
-  let headers = [ "Status: 200" ;
-                  "Content-Type: text/plain" ; ]
+let response =
+  let nil = Seq.empty |> List.of_seq in
+  let headers = "Status: 200" ::
+                  "Content-Type: text/plain" ::
+                    nil
   in
   let crlf = "\r\n" in
   let body = "Your wish is granted.  Long live Jambi." in
-  String.concat "" [ String.concat crlf headers ;
-                     crlf ;
-                     crlf ;
-                     body ;
-                     crlf ; ]
+  String.concat "" @@ String.concat crlf headers
+                      :: crlf
+                      :: crlf
+                      :: body
+                      :: crlf
+                      :: nil
 
 (* Local Variables: *)
 (* mode: tuareg *)
