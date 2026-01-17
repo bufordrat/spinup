ab8f88b92a2c4cf6ed2161461d73b52ab046e4c8
Matt Teichman <teichman@uchicago.edu>
2026-01-17 17:18:41 -0600

    oops, pound pound

--- lib/templates/lib/message.ml
+++ lib/templates/lib/message.ml
@@ -1,13 +1,13 @@
 let response =
-  let sayings = #[left] "Your wish is granted.  Long live Jambi." ;
+  let sayings = ##[left] "Your wish is granted.  Long live Jambi." ;
                   "Why don't you take a picture?  It'll last longer." ;
                   "Mecca lecca hi, mecca hiney ho." ;
                   "You don't wanna get mixed up with a guy like \
                    me. I'm a loner, Dottie. A rebel." ;
-                  "I know you are, but what am I?" ; #[right]
+                  "I know you are, but what am I?" ; ##[right]
   in
-  let headers = #[left] "Status: 200" ;
-                  "Content-Type: text/plain" ; #[right]
+  let headers = ##[left] "Status: 200" ;
+                  "Content-Type: text/plain" ; ##[right]
   in
   let crlf = "\r\n" in
   let idx =
@@ -15,11 +15,11 @@ let response =
     Random.full_int max_int mod List.length sayings
   in
   let body = List.nth sayings idx in
-  String.concat "" #[left] String.concat crlf headers ;
+  String.concat "" ##[left] String.concat crlf headers ;
                      crlf ;
                      crlf ;
                      body ;
-                     crlf ; #[right]
+                     crlf ; ##[right]
 
 (* Local Variables: *)
 (* mode: tuareg *)
